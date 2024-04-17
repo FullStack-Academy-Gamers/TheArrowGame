aa6b4a17658454a0b399044d2a038153b68faadf
boom-sm <santiparp1806754@gmail.com>
2024-04-17 18:32:20 -0400

    fixing the package and server

--- new/server/index.js
+++ new/server/index.js
@@ -1,8 +1,28 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = 3000; // server hardcoded to run on port 3000. 
+const PORT = process.env.PORT || 3000; // server hardcoded to run on port 3000. 
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`Server running on http://localhost:${PORT} in ${process.env.NODE_ENV} mode.`);
+}).on('error', error => {
+  console.error('Error occurred starting the server: ', error);
+});
+
+process.on('SIGINT', () => {
+  console.info('SIGINT signal received.');
+  console.log('Closing http server.');
+  server.close(() => {
+    console.log('Http server closed.');
+    process.exit(0);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.info('SIGTERM signal received.');
+  console.log('Closing http server.');
+  server.close(() => {
+    console.log('Http server closed.');
+    process.exit(0);
+  });
 });
